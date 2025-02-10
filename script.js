25d8d24b3b406343d3d6e105c0752bec63fd8c83
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-02-10 13:07:02 +0500

    Add getHumanChoice function

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ document.write("Hello World!");
 //generate a number from 0 to 1
 function getComputerChoice() {
     let pc = Math.random();
+    //if the number is 0 return rock
     if(pc = 0) {
         console.log("rock");
     }
+    //if the number is less than 0.5 return scissors
     else if (pc < 0.5) {
         console.log("scissors");
     }
+    //if the nuber is more than 0.5 return paper
     else {
         console.log("paper");
     }
 }
-getComputerChoice();
-//if the number is 0 return rock
-//if the number is less than 0.5 return scissors
-//if the nuber is more than 0.5 return paper
+// getComputerChoice();
+function getHumanChoice() {
+    let choice = prompt('Make you choice!')
+    return choice;
+}
+
+getHumanChoice();
