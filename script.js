639b6b3f0446edc38b072fc5478473a512b64788
Max Kamenskii <80255339+MaxKamenskii@users.noreply.github.com>
2025-02-11 11:05:13 +0500

    Add playRound function

--- script.js
+++ script.js
@@ -2,25 +2,65 @@ console.log("Hello world!");
 document.write("Hello World!");
 
 //generate a number from 0 to 1
+
 function getComputerChoice() {
-    let pc = Math.random();
+    let computerChoice = Math.floor(Math.random() * 3);
+    // console.log(`Computer choice is ${computerChoice}`);
     //if the number is 0 return rock
-    if(pc = 0) {
-        console.log("rock");
+    if(computerChoice === 0) {
+        // console.log(`Computer choice for rock is ${computerChoice}`)
+        computerChoice = 'rock';
     }
     //if the number is less than 0.5 return scissors
-    else if (pc < 0.5) {
-        console.log("scissors");
+    else if (computerChoice === 2) {
+        // console.log(`Computer choice for scissors is ${computerChoice}`)
+        computerChoice = 'scissors';
     }
     //if the nuber is more than 0.5 return paper
-    else {
-        console.log("paper");
+    else if (computerChoice === 1) {
+        // console.log(`Computer choice for paper is ${computerChoice}`)
+        computerChoice = 'paper';
     }
+    return computerChoice;
 }
 // getComputerChoice();
 function getHumanChoice() {
-    let choice = prompt('Make you choice!')
-    return choice;
+    let humanChoice = prompt('Make you choice!')
+    // console.log(`getHumanChoice is ${humanChoice}`)
+    return humanChoice;
 }
+// getHumanChoice();
 
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+
+function playRound(humanChoice, computerChoice) {
+
+    // Put the choice to lower case before comparison
+    humanChoice.toLowerCase();
+    // computerChoice.toLowerCase();
+        if(humanChoice === 'rock' && computerChoice === 'scissors') {
+            humanScore++;
+        } else if(humanChoice === 'rock' && computerChoice === 'paper') {
+            computerScore++;
+        } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+            computerScore++;
+        } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+            humanScore++;        
+        } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+            humanScore++;
+        }
+        console.log(`Human score is ${humanScore}`)
+        console.log(`Computer score is ${computerScore}`)
+    
+};
+const humanSelection = getHumanChoice();
+console.log(`humanSelection is ${humanSelection}`);
+const computerSelection = getComputerChoice();
+console.log(`computerSelection is ${computerSelection}`);
+
+
+playRound(humanSelection, computerSelection);
+
+
